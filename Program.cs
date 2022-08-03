4ca0644fcc0d24d63e18ae69189e98526e43e150
Octa <octavio.macieira@aarim.com>
2022-08-03 20:38:29 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp, velMed;
+            double cons;
+
+            Console.WriteLine("Insira o tempo de viagem: ");
+            temp = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a velocidade média: ");
+            velMed = int.Parse(Console.ReadLine());
+
+            cons = (double) (temp * velMed) / 12;
+
+            Console.WriteLine(cons.ToString("F3", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
